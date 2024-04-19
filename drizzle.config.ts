247c20109da4bc272c4e1f7df0a312ac60c8f804
André Miranda <aegis.andre@gmail.com>
2024-04-19 15:40:03 -0300

    build: add drizzle orm and expo sqlite

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "drizzle-kit"
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  driver: "expo",
+  verbose: true,
+  strict: true
+} satisfies Config
