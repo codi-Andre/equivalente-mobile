7a0b92250174b34ad20b9427d5b180049aea7ffc
André Miranda <aegis.andre@gmail.com>
2024-04-20 03:16:23 -0300

    refactor: add migrations to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,15 @@
-import { useFonts } from "expo-font"
+import { useLoadAssets } from "@/hooks/use-load-assets"
 import { Slot } from "expo-router"
-import * as SplashScreen from "expo-splash-screen"
-import { useEffect } from "react"
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary
 } from "expo-router"
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
-
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
-    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
-    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf")
-  })
+  const { isLoaded } = useLoadAssets()
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error
-  }, [error])
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync()
-    }
-  }, [loaded])
-
-  if (!loaded) {
-    return null
-  }
+  if (!isLoaded) return null
 
   return <Slot />
 }
